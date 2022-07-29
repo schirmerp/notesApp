5becef09a36e6999096e7e0b3824cf6fecb7857f
Joe <schirmer.pc@gmail.com>
2022-07-29 16:51:56 -0400

    changed everything again

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
-#simple note application im working on written in python. 
-#It will implement a local text file as a DB
+#simple sticky note application
+
+#built in functionality to save note, append to previous notes
+#and change color of the note.
+
+#Whil working on labs and other projects I found myself often 
+#flipping back and forth between app/windows. If i needed to 
+#include multpile values from one window in the other i would 
+#typicall put the values in a text document for quick reference
+ the problem was when i switched windows to look at anything else it would require me to bring the text doc back to the top of the display. 
+
+ Sticky notes are simple notes that can be "stuck" to the top of the display so that you wont have to dig around under all your open apps to get your displayed note back!
--- stickyN.py
+++ stickyN.py
@@ -1,5 +1,11 @@
 from tkinter import *
+import os
+from tkinter.simpledialog import askstring
+from datetime import date
+from tkinter.colorchooser import askcolor
 
+today = date.today()
+d1 = today.strftime("%m/%d/%y")
 
 w = Tk()
 
@@ -9,20 +15,43 @@ def sn():
     sn.attributes('-topmost', True)
     sn_text = Text(sn, bg='yellow', font=('Courier_New', 15, 'bold'))
     sn_text.pack(fill='both')
-    b2 = Button(sn, text="Save")
+    
+    st= sn_text.get(1.0, 'end')
+    
+    def save_note():
+        n = askstring("Save", "Enter filename for note to save:")
+        path = os.path.abspath(os.getcwd())
+        filename = f"{path}/{n}.txt"
+        with open(filename, 'a') as f:
+            f.write(str(d1) + '\n' + sn_text.get(1.0, 'end') + '\n')
+
+    b2 = Button(sn, text="Save", command=save_note)
+    b2.place(rely=1.0,relx=1.0, anchor='se')    
+    def cc():
+        colors = askcolor(title="Choose Color")
+        sn_text.configure(bg=colors[1])
+
     m= Menu(sn)
-    save = Menu(m)
-    save.add_command(label='Save', command ='save')
-    save.add_separator()
-    save.add_command(label='New', command= 'new')
-    m.add_cascade(label='File', m=save)
+    n_color = Menu(m)
+    n_color.add_command(label="Color", command= cc)
+    m.add_cascade(label="Change Color", m=n_color)
     sn.config(menu=m)
     
+# add  color selection option to menu 
+# add save option for note
 
 
 
 
-n_t = StringVar()
 b1 = Button(w, text='New Note', command= sn).grid(row=0, column=0)
 
 w.mainloop()
+
+
+# m= Menu(sn)
+#     save = Menu(m)
+#     save.add_command(label='Save', command ='save')
+#     save.add_separator()
+#     save.add_command(label='New', command= 'new')
+#     m.add_cascade(label='File', m=save)
+#     sn.config(menu=m)
